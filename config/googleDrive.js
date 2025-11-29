45fbc7b39c50b1e518ad231e4f9a4c287f821abc
Phuchermes <phucdjhot888@gmail.com>
2025-11-29 13:49:49 +0700

    Initial commit

--- config/googleDrive.js
+++ config/googleDrive.js
@@ -1,7 +1,7 @@
 const { google } = require("googleapis");
 const path = require("path");
 
-const KEYFILEPATH = path.join(__dirname, "../magnetic-runway-479704-b7-a3557072f35c.json");
+const KEYFILEPATH = path.join(__dirname, "./magnetic-runway-479704-b7-a3557072f35c");
 const SCOPES = ["https://www.googleapis.com/auth/drive"];
 
 const auth = new google.auth.GoogleAuth({
