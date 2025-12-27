9a89c5bcf4641d3effbb76d4f13dbde77d7708e0
Phuchermes <phucdjhot888@gmail.com>
2025-12-28 02:43:15 +0700

    Initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,18 +6,20 @@ module.exports = {
       exec_mode: "cluster",
       instances: "max",
       max_memory_restart: "300M",
+      autorestart: true,
+      listen_timeout: 10000,
+      kill_timeout: 5000,
+      watch: false,
+
       cwd: "/var/www/backend-vdocs",
       env: {
         PORT: 3000,
         MONGO_URI: "mongodb://127.0.0.1:27017/fileuploads",
         JWT_SECRET: "my_super_secret_key"
       },
+      error_file: "./logs/err.log",
+      out_file: "./logs/out.log",
+      log_date_format: "YYYY-MM-DD HH:mm:ss"
     },
-    {
-      name: "cleanup",
-      script: "worker-cleanup.js",
-      exec_mode: "fork",
-      cron_restart: "0 3 * * *"
-    }
   ],
 };
