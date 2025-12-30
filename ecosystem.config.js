f8899cc9471b307dad1ec2d339ac81291d11395c
Phuchermes <phucdjhot888@gmail.com>
2025-12-30 11:49:29 +0700

    Initial commit

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,6 +3,7 @@ module.exports = {
     {
       name: "backendvdocs",
       script: "./index.js",
+      cwd: "/var/www/backend-vdocs",
       
       exec_mode: "cluster",
       instances: "2",
@@ -10,15 +11,14 @@ module.exports = {
       max_memory_restart: "512M",
       autorestart: true,
 
-      listen_timeout: 30000,
+      listen_timeout: 8000,
       kill_timeout: 10000,
 
       watch: false,
-      cwd: "/var/www/backend-vdocs",
+      node_args: "--max-old-space-size=384",
+      
       env: {
-        PORT: 3000,
-        MONGO_URI: "mongodb://127.0.0.1:27017/fileuploads",
-        JWT_SECRET: "my_super_secret_key"
+        NODE_ENV: "production"
       },
       merge_logs: true,
       log_date_format: "YYYY-MM-DD HH:mm:ss"
