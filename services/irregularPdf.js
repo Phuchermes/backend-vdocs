e4afc499ccf5056219db3e5727cb8d342d0cf078
Phuchermes <phucdjhot888@gmail.com>
2026-01-05 23:43:18 +0700

    Initial commit

--- services/irregularPdf.js
+++ services/irregularPdf.js
@@ -28,43 +28,45 @@ exports.generateIrregularPDF = async ({
   const page = pdfDoc.getPages()[0];
   const { height } = page.getSize();
     const fontSize = 15;
+    const safeText = (v) =>
+  typeof v === "string" ? v : v != null ? String(v) : "";
 
-    page.drawText(formData.location, { x: 120, y: height - 226, size: fontSize, font });
-    page.drawText(formData.time, { x: 90, y: height - 166, size: fontSize, font });
-    page.drawText(formData.day, { x: 225, y: height - 166, size: fontSize, font });
-    page.drawText(formData.month, { x: 305, y: height - 166, size: fontSize, font });
-    page.drawText(formData.year, { x: 380, y: height - 166, size: fontSize, font });
+    page.drawText(safeText(formData.location), { x: 120, y: height - 226, size: fontSize, font });
+    page.drawText(safeText(formData.time), { x: 90, y: height - 166, size: fontSize, font });
+    page.drawText(safeText(formData.day), { x: 225, y: height - 166, size: fontSize, font });
+    page.drawText(safeText(formData.month), { x: 305, y: height - 166, size: fontSize, font });
+    page.drawText(safeText(formData.year), { x: 380, y: height - 166, size: fontSize, font });
 
-    page.drawText(formData.name1, { x: 90, y: height - 300, size: fontSize, font });
-    page.drawText(formData.name2, { x: 90, y: height - 330, size: fontSize, font });
-    page.drawText(formData.dept1, { x: 440, y: height - 300, size: fontSize, font });
-    page.drawText(formData.dept2, { x: 440, y: height - 330, size: fontSize, font });
+    page.drawText(safeText(formData.name1), { x: 90, y: height - 300, size: fontSize, font });
+    page.drawText(safeText(formData.name2), { x: 90, y: height - 330, size: fontSize, font });
+    page.drawText(safeText(formData.dept1), { x: 440, y: height - 300, size: fontSize, font });
+    page.drawText(safeText(formData.dept2), { x: 440, y: height - 330, size: fontSize, font });
 
-    page.drawText(formData.info, { x: 210, y: height - 370, size: fontSize, font });
-    page.drawText(formData.tag, { x: 92, y: height - 401, size: fontSize, font });
+    page.drawText(safeText(formData.info), { x: 210, y: height - 370, size: fontSize, font });
+    page.drawText(safeText(formData.tag), { x: 92, y: height - 401, size: fontSize, font });
 
-    page.drawText(formData.location, { x: 145, y: height - 434, size: fontSize, font });
-    page.drawText(formData.from, { x: 280, y: height - 434, size: fontSize, font });
-    page.drawText(formData.to, { x: 420, y: height - 434, size: fontSize, font });
+    page.drawText(safeText(formData.location), { x: 145, y: height - 434, size: fontSize, font });
+    page.drawText(safeText(formData.from), { x: 280, y: height - 434, size: fontSize, font });
+    page.drawText(safeText(formData.to), { x: 420, y: height - 434, size: fontSize, font });
 
-    page.drawText(formData.info, { x: 90, y: height - 467, size: fontSize, font });
-    page.drawText(formData.cause, { x: 100, y: height - 500, size: fontSize, font });
-    page.drawText(formData.resolve, { x: 180, y: height - 535, size: fontSize, font });
+    page.drawText(safeText(formData.info), { x: 90, y: height - 467, size: fontSize, font });
+    page.drawText(safeText(formData.cause), { x: 100, y: height - 500, size: fontSize, font });
+    page.drawText(safeText(formData.resolve), { x: 180, y: height - 535, size: fontSize, font });
 
-    page.drawText(formData.rep, { x: 228, y: height - 602, size: fontSize, font });
+    page.drawText(safeText(formData.rep), { x: 228, y: height - 602, size: fontSize, font });
 
-    page.drawText(formData.dept1, { x: 280, y: height - 682, size: fontSize, font });
-    page.drawText(formData.dept2, { x: 450, y: height - 682, size: fontSize, font });
+    page.drawText(safeText(formData.dept1), { x: 280, y: height - 682, size: fontSize, font });
+    page.drawText(safeText(formData.dept2), { x: 450, y: height - 682, size: fontSize, font });
 
   // checkbox
   if (checkboxes.damaged)
-    page.drawText("X", { x: 178, y: height - 114, size: 14, font });
+    page.drawText(safeText("X"), { x: 178, y: height - 114, size: 14, font });
   if (checkboxes.wet)
-    page.drawText("X", { x: 294, y: height - 114, size: 14, font });
+    page.drawText(safeText("X"), { x: 294, y: height - 114, size: 14, font });
   if (checkboxes.leaking)
-    page.drawText("X", { x: 420, y: height - 114, size: 14, font });
+    page.drawText(safeText("X"), { x: 420, y: height - 114, size: 14, font });
   if (checkboxes.other)
-    page.drawText("X", { x: 526, y: height - 114, size: 14, font });
+    page.drawText(safeText("X"), { x: 526, y: height - 114, size: 14, font });
 
   // signatures
   if (signatures.sig1) {
