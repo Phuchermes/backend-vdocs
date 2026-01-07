e0974bee9a7ae79a3cb05cb38122b622d8784764
Phuchermes <phucdjhot888@gmail.com>
2026-01-07 22:22:27 +0700

    Initial commit

--- services/avihPdf.js
+++ services/avihPdf.js
@@ -58,12 +58,14 @@ exports.generateAVIHPDF = async ({
         if (signavih.sig1) {
           const img = await pdfDoc.embedPng(fs.readFileSync(signavih.sig1.tmpPath));
           page.drawImage(img, { x: 500, y: baseY , width: img.width * 0.07, height: img.height * 0.07 });
+          console.log("SIG1 PATH:", signavih.sig1.tmpPath);
         }
 
         // Draw NV2 signature
         if (signavih.sig2) {
           const img = await pdfDoc.embedPng(fs.readFileSync(signavih.sig2.tmpPath));
           page.drawImage(img, { x: 570, y: baseY, width: img.width * 0.07, height: img.height * 0.07 });
+          console.log("SIG2 PATH:", signavih.sig2.tmpPath);
         }
     }
     const form = pdfDoc.getForm();
